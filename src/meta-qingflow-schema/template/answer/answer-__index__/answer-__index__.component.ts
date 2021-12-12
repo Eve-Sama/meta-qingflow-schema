7390b7f32775e2fd141a1810c4d7e7f84beec50c
Eve-Sama <948832626@qq.com>
2021-12-12 21:28:03 +0800

    init

---
+++ src/meta-qingflow-schema/template/answer/answer-__index__/answer-__index__.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './answer-<%= index %>.component.html',
+  styleUrls: ['./answer-<%= index %>.component.css'],
+})
+export class Answer<%= index %>Component {
+  
+}
---
+++ src/meta-qingflow-schema/template/question/question-__index__/question-__index__.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './question-<%= index %>.component.html',
+  styleUrls: ['./question-<%= index %>.component.css'],
+})
+export class Question<%= index %>Component {
+  
+}
