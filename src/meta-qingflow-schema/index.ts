32bb8a33b9fc200edac15ab117bffefa716dd7bf
Eve-Sama <948832626@qq.com>
2022-03-13 02:01:03 +0800

    chore: add yalc

--- src/meta-qingflow-schema/index.ts
+++ src/meta-qingflow-schema/index.ts
@@ -4,7 +4,6 @@ import { generateFiles, addImportStatement } from './utils';
 export function metaQingflowSchema(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const { index, question, answer, analyse } = options;
-    console.log(options, `options`);
     const questionSource = generateFiles(options, './template/question', 'src/app/QAA/question');
     const answerSource = generateFiles(options, './template/answer', 'src/app/QAA/answer');
     const analyseSource = generateFiles(options, './template/analyse', 'src/assets/md/analyse');
