a469cc6712045a1075f8ae876c22333bb8a857ac
Eve-Sama <948832626@qq.com>
2022-03-23 16:51:30 +0800

    refactor: 优化打包命令

--- README.md
+++ README.md
@@ -11,7 +11,6 @@
 本地开发时, 使用了[yalc](https://juejin.cn/post/7033400734746066957). 在完成本地开发后, 需要先进行打包.
 ```bash
 npm run build
-npm run copy
 ```
 
 之后进入打包目录进行本地发布.
@@ -20,7 +19,7 @@ cd dist/meta-qingflow-schema
 npm run publish:dev
 ```
 
-之后在`meta-qingflow`项目中执行以下代码, 加载该插件.
+之后在`meta-qingflow`项目中执行以下代码(执行一次即可), 加载该插件.
 ```bash
 yalc add @eve-sama/meta-qingflow-schema
 ```
--- src/add/index.ts
+++ src/add/index.ts
@@ -3,24 +3,21 @@ import { generateFiles, addImportStatement } from './utils';
 
 export default function (options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const { index, question, answer, analyse } = options;
+    const { index, answer, analyse } = options;
     const questionSource = generateFiles(options, './template/question', 'src/app/QAA/question');
     const answerSource = generateFiles(options, './template/answer', 'src/app/QAA/answer');
     const analyseSource = generateFiles(options, './template/analyse', 'src/assets/md/analyse');
     const describeSource = generateFiles(options, './template/describe', 'src/assets/md/describe');
-    const sourceArr: Source[] = [describeSource];
+    const sourceList: Source[] = [];
     const modulePath = 'src/app/QAA/QAA.config.ts';
-    if (question) {
-      sourceArr.push(questionSource);
-      addImportStatement(
-        tree,
-        modulePath,
-        `Question${index}Component`,
-        `./question/question-${index}/question-${index}.component`
-      );
+    if (analyse) {
+      sourceList.push(analyseSource);
     }
+    // 在这里push是为了让创建文件时question和answer排在一起, analyse和describe排在一起
+    sourceList.push(describeSource);
+    sourceList.push(questionSource);
     if (answer) {
-      sourceArr.push(answerSource);
+      sourceList.push(answerSource);
       addImportStatement(
         tree,
         modulePath,
@@ -28,11 +25,14 @@ export default function (options: any): Rule {
         `./answer/answer-${index}/answer-${index}.component`
       );
     }
-    if (analyse) {
-      sourceArr.push(analyseSource);
-    }
+    addImportStatement(
+      tree,
+      modulePath,
+      `Question${index}Component`,
+      `./question/question-${index}/question-${index}.component`
+    );
     const ruleArr: Rule[] = [];
-    sourceArr.forEach(v => ruleArr.push(mergeWith(v)));
+    sourceList.forEach(v => ruleArr.push(mergeWith(v)));
     return chain(ruleArr);
   };
 }
