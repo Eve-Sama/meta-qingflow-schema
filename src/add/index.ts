cc75fbc5214064a348703aa3e08bcd24f54121a7
Eve-Sama <948832626@qq.com>
2022-03-23 21:31:43 +0800

    feat: 支持自动添加新创建的组件在特定的数组中

--- src/add/index.ts
+++ src/add/index.ts
@@ -1,5 +1,25 @@
-import { Rule, SchematicContext, Tree, mergeWith, Source, chain } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, mergeWith, Source } from '@angular-devkit/schematics';
+import { Project, SyntaxKind } from 'ts-morph';
 import { generateFiles, addImportStatement } from './utils';
+const modulePath = 'src/app/QAA/QAA.config.ts';
+
+/**
+ * 在特定数组中插入元素
+ * @param arrayName 数组名
+ * @param insertText 插入的文本内容
+ */
+function insertArrayItem(arrayName: string, insertText: string): void {
+  const project = new Project();
+  const sourceFile = project.addSourceFileAtPath(modulePath);
+  const declaration = sourceFile.getVariableDeclaration(arrayName);
+  const arrayLiteralExpress = declaration!.getInitializerIfKindOrThrow(SyntaxKind.ArrayLiteralExpression);
+  const elements = arrayLiteralExpress.getElements();
+  const lastElement = elements[elements.length - 1];
+  // 新元素插入的位置, 需要计算当前最后个数组元素的起点位置+元素宽度, 还有4个字符的宽度我也不知道怎么来的, +上就对了
+  const startPosition = lastElement.getPos() + lastElement.getText().length + 4;
+  sourceFile.insertText(startPosition, `\n\t${insertText},`);
+  sourceFile.saveSync();
+}
 
 export default function (options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
@@ -9,7 +29,6 @@ export default function (options: any): Rule {
     const analyseSource = generateFiles(options, './template/analyse', 'src/assets/md/analyse');
     const describeSource = generateFiles(options, './template/describe', 'src/assets/md/describe');
     const sourceList: Source[] = [];
-    const modulePath = 'src/app/QAA/QAA.config.ts';
     if (analyse) {
       sourceList.push(analyseSource);
     }
@@ -31,8 +50,9 @@ export default function (options: any): Rule {
       `Question${index}Component`,
       `./question/question-${index}/question-${index}.component`
     );
+    insertArrayItem('QUESTION', `Question${index}Component`);
     const ruleArr: Rule[] = [];
     sourceList.forEach(v => ruleArr.push(mergeWith(v)));
-    return chain(ruleArr);
+    return chain([]);
   };
 }
